ea352365909798ae63b41a7e183ac5ff123b4e58
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-11-09 17:00:27 +0700

    Menambahkan addNode

--- Singly_Linked_List/Program.cs
+++ Singly_Linked_List/Program.cs
@@ -8,8 +8,30 @@
         public string nama;
         public Node next;
     }
-    
-    
+   
+    class List
+    {
+    Node START;
+    public List()
+    {
+        START = null;
+    }
 
+    public void addNode()/*Method untuk menambahkan sebuah Node kedalam list*/
+    {
+        int nim;
+        string nm;
+        Console.Write("\nMasukkan nomer Mahasiswa: ");
+        nim = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\nMasukkan nama Mahasiswa: ");
+        nm = Console.ReadLine();
+        Node nodeBaru = new Node();
+        nodeBaru.noMhs = nim;
+        nodeBaru.nama = nm;
+
+    }
 }
+
+
+
 
