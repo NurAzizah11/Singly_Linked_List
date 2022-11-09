a3c440a0068987498cc0374ee7fa341686e60b8d
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-11-09 19:11:22 +0700

    Menambahkan case 1, 2, 3, 4 dan 5

--- Singly_Linked_List/Program.cs
+++ Singly_Linked_List/Program.cs
@@ -139,11 +139,68 @@ class Program
                 {
                     case '1':
                         {
-                            obj.addNode
+                            obj.addNode();
                         }
+                        break;
+                    case '2':
+                        {
+                            if (obj.ListEmpty())
+                            {
+                                Console.WriteLine("\nList Kosong");
+                                break;
+                            }
+                            Console.WriteLine("\nMasukkan nomor mahasiswa yang akan dihapus: ");
+                            int nim = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine();
+                            if (obj.delNode(nim) == false)
+                                Console.WriteLine("\nData tidak ditemukan.");
+                            else
+                                Console.WriteLine("Data dengan nomor mahasiswa " + nim + " dihapus ");
+                        }
+                        break;
+                    case '3':
+                        {
+                            obj.traverse();
+                        }
+                        break;
+                    case '4':
+                        {
+                            if (obj.ListEmpty() == true)
+                            {
+                                Console.WriteLine("\nList Kosong |");
+                                break;
+                            }
+                            Node previous, current;
+                            previous = current = null;
+                            Console.Write("\nMasukkan nomor mahasiswa yang akan dicari: ");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            if (obj.Search(num, ref previous, ref current) == false)
+                                Console.WriteLine("\nData tidak ditemukan.");
+                            else
+                            {
+                                Console.WriteLine("\nData ketemu");
+                                Console.WriteLine("\nNomor Mahasiswa: " + current.noMhs);
+                                Console.WriteLine("\nNama: " + current.nama);
+                            }
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("\nPilihan tidak valid");
+                            break;
+                        }
+
                 }
             }
+
+            catch
+            {
+                Console.WriteLine("\nCheck nilai yang anda masukkan.");
+            }
         }
+
     }
 }
 
